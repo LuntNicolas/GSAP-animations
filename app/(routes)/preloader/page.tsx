fa0847831bcca844075640ebe2c0cc16c000919e
luntNicolas <lunt.lennart@gmail.com>
2026-04-11 12:51:00 +0200

    first commit

---
+++ app/(routes)/preloader/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {useIntersection} from "next/dist/client/use-intersection";
+
+const Page = () => {
+    return (
+        <section>
+            <div className="preloader-backdrop"></div>
+        </section>
+    )
+}
+export default Page
